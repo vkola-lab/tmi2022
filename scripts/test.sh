9616666b9388090b750639aabf0a1a000e590a97
YZ <zhyzh91@gmail.com>
2021-09-07 00:21:23 -0400

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,12 @@
 export CUDA_VISIBLE_DEVICES=0
-python -W ignore main.py \
+python main.py \
 --n_class 3 \
---data_path "/scratch2/zheng/cptac_data/" \
---val_set "cptac_lung_val1.txt" \
---model_path "/scratch2/zheng/kidney_fibrosis_patch_based/Github/deep_globe/saved_models/" \
---log_path "/scratch2/zheng/kidney_fibrosis_patch_based/GLNet/deep_globe/runs/" \
+--data_path "path_ti_graph_data" \
+--val_set "test_set.txt" \
+--model_path "../graph_transformer/saved_models/" \
+--log_path "../graph_transformer/runs/" \
 --task_name "GraphCAM" \
 --batch_size 1 \
 --test \
 --log_interval_local 6 \
---resume "/scratch2/zheng/kidney_fibrosis_patch_based/Github/deep_globe/saved_models_val1/GraphCAM.pth"
+--resume "../graph_transformer/saved_models/GraphCAM.pth"
