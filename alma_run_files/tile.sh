45ed02ec94679f508c221e72b99c45f354be44c0
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-06-20 10:21:20 +0100

    run sh file

--- alma_run_files/tile.sh
+++ alma_run_files/tile.sh
@@ -10,9 +10,9 @@
 
 module load anaconda/3
 source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
-conda activate torch-gpu
+conda activate card
 cd /home/ofourkioti/Projects/tmi2022/
 
-python src/tile_WSI.py -s 512 -e 0 -j 32 -B 50 -M 20 -o  /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/TCGA_LUNG/  /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/TCGA_data/TCGA_flat/*.svs
+python src/tile_WSI.py -s 512 -e 0 -j 32 -B 50 -M 20 -o  /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam_16/  /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/camelyon_data/camelyon_slides/slides/*.tif
 
 
