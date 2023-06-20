82a08040d326c05cc2f7d7206679eced22e3acf6
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-06-20 13:55:26 +0100

    alma

--- alma_run_files/tile.sh
+++ alma_run_files/tile.sh
@@ -15,5 +15,5 @@ cd /home/ofourkioti/Projects/tmi2022/
 
 #python src/tile_WSI.py -s 512 -e 0 -j 32 -B 50 -M 20 -o  /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam_16/ "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/camelyon_data/camelyon_slides/slides/*.tif"
 
-python src/tile_WSI.py -s 512 -e 0 -j 12 -B 50 -M 20 -o  /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/TCGA_LUNG/tiles/ "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/TCGA_data/TCGA_flat/*.svs"
+python src/tile_WSI.py -s 512 -e 0 -B 50 -M 20 -o  /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/TCGA_LUNG/tiles/ "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/TCGA_data/TCGA_flat/*.svs"
 
