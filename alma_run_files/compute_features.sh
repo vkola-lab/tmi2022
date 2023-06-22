a648d20b473b3c32168003db5cc7bae54e7dc6d2
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-06-22 13:09:01 +0100

    compute feats for camelyon

--- alma_run_files/compute_features.sh
+++ alma_run_files/compute_features.sh
@@ -15,4 +15,4 @@ conda activate card
 cd /home/ofourkioti/Projects/tmi2022/feature_extractor
 
 
-python build_graphs.py --weights "model.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/datasets/camelyon_data/size_256/patches/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/res-18-feats/"
+python compute_feats_res.py --weights "model.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/datasets/camelyon_data/size_256/patches/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/res-18-feats/"
