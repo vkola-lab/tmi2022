89c8dcffc186e8b22e5fda1f1573d13c6f6b0d32
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-06-26 13:12:34 +0100

    compute feats cam 17

--- alma_run_files/compute_features.sh
+++ alma_run_files/compute_features.sh
@@ -3,9 +3,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=50:00:00
-#SBATCH --output=/home/ofourkioti/Projects/tmi2022/results/build_graphs.out
-#SBATCH --error=/home/ofourkioti/Projects/tmi2022/results/build_graphs.err
+#SBATCH --time=100:00:00
+#SBATCH --output=/home/ofourkioti/Projects/tmi2022/results/compute_feats_cam-17.out
+#SBATCH --error=/home/ofourkioti/Projects/tmi2022/results/compute_feats_cam.err
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu
 
@@ -15,5 +15,5 @@ conda activate card
 cd /home/ofourkioti/Projects/tmi2022/feature_extractor
 
 
-#python compute_feats_res.py --weights "model.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/datasets/camelyon_data/size_256/patches/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/res-18-feats/" --slide_dir /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/camelyon_data/camelyon_slides/slides
-python build_graphs.py --weights "model.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/TCGA-LUNG/tiles/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/graphs"
+python compute_feats_res.py --weights "DSMIL_extractors/20x/model-v0.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/camelyon17/patches/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-17/feats/" --slide_dir /d/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/camelyon17/images/
+#python build_graphs.py --weights "model.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/TCGA-LUNG/tiles/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/TCGA-LUNG/graphs"
