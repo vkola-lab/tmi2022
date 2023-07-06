31831ca0ce8b588bb1d9d3c721be948c9a77353c
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-07-06 13:16:55 +0100

    path

--- feature_extractor/compute_feats_gtp.py
+++ feature_extractor/compute_feats_gtp.py
@@ -99,10 +99,11 @@ def adj_matrix(wsi_coords):
     patch_distances = pairwise_distances(wsi_coords, metric='euclidean', n_jobs=1)
     neighbor_indices = np.argsort(patch_distances, axis=1)[:, :16]
 
-    sum=0
+
     for i in range(total-1):
         x_i, y_i = wsi_coords[i][0], wsi_coords[i][1]
         indices = neighbor_indices[i]
+        sum = 0
         for j in range(i+1, indices.shape[0]):
             x_j, y_j = wsi_coords[j][0], wsi_coords[j][1]
             if abs(int(x_i)-int(x_j)) <=1536 and abs(int(y_i)-int(y_j)) <= 1536:
