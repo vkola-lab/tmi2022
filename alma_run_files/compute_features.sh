8c909ff57c1b679baca7c8faea744111a99dc3d2
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-07-11 17:58:24 +0100

    compute feats

--- alma_run_files/compute_features.sh
+++ alma_run_files/compute_features.sh
@@ -9,10 +9,13 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpuhm
 
-module load anaconda/3
-source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
-conda activate dl_torch
-cd /home/ofourkioti/Projects/tmi2022/feature_extractor
+
+module use /opt/software/easybuild/modules/all/
+module load Mamba
+source ~/.bashrc
+mamba activate  dl_torch
+cd /home/ofourkioti/Projects/tmi2022/
+
 
 python build_graphs.py --weights "DSMIL_extractors/20x/model-v0.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/tiles/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/graphs/"
 #python build_graphs.py --weights "model.pth" --dataset "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/TCGA-LUNG/tiles/*" --output "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/TCGA-LUNG/graphs"
