ca9f49f201a208aa8134b99fb84a6633bb1f211e
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-07-11 21:56:13 +0100

    changes for tiling

--- feature_extractor/compute_feats_res.py
+++ feature_extractor/compute_feats_res.py
@@ -122,7 +122,6 @@ def generate_values_resnet(images, Idx, dist="wsi_feats"):
 def adj_matrix(wsi_coords,wsi_feats):
     total = wsi_coords.shape[0]
 
-
     patch_distances = pairwise_distances(wsi_coords, metric='euclidean', n_jobs=1)
     neighbor_indices = np.argsort(patch_distances, axis=1)[:, :16]
     values=[]
@@ -132,17 +131,18 @@ def adj_matrix(wsi_coords,wsi_feats):
         x_i, y_i = wsi_coords[i][0], wsi_coords[i][1]
         indices = neighbor_indices[i]
         sum = 0
-        for j in range(i, indices.shape[0]):
+        for j in range(indices.shape[0]):
             x_j, y_j = wsi_coords[j][0], wsi_coords[j][1]
             if abs(int(x_i)-int(x_j)) <=1024 and abs(int(y_i)-int(y_j)) <= 1024:
                 m1 = np.expand_dims(wsi_feats[int(i)], axis=0)
                 m2 = np.expand_dims(wsi_feats[int(j)], axis=0)
                 value = distance.cdist(m1.reshape(1, -1), m2.reshape(1, -1), 'cosine')[0][0]
-                values.append(value)
+                values.append(np.exp(-value))
                 adj_coords.append((i,j))
                 sum+=1
             if sum==5:
                 break
+
     print (np.array(adj_coords).shape, np.array(values).shape)
     return np.array(adj_coords), np.array(values)
 
