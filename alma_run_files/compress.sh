3f887060c6b6a3a7c9f4b5b4df70b584dfff8e92
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-07-15 17:44:18 +0100

    comrpess

---
+++ alma_run_files/compress.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=datatransfer_test
+#SBATCH -o comp.%J.out
+#SBATCH -e comp.%J.err
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=100:00:00
+#SBATCH --cpus-per-task=24
+
+
+#srun rsync -avP   /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/ovarian_cancer/patches/* /data/rds/DBI/DUDBI/DYNCESYS/OlgaF/ovarian_cancer/patches/
+
+#srun rsync -avP /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/tiles/* /data/rds/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/tiles/
+#srun rsync -avP /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/*  /data/rds/DBI/DUDBI/DYNCESYS/OlgaF/slides/
+pigz -9 /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/tiles/*
