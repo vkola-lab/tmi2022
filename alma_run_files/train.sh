424ab2e3bb86be59add0a03405a638ddeb54502f
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-07-15 16:58:06 +0100

    file sizes

--- alma_run_files/train.sh
+++ alma_run_files/train.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --time=100:00:00
-#SBATCH --output=/home/ofourkioti/Projects/tmi2022/results/train_cam16.out
+#SBATCH --output=/home/ofourkioti/Projects/tmi2022/results/train.out
 #SBATCH --error=/home/ofourkioti/Projects/tmi2022/results/train.err
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpuhm
@@ -15,12 +15,13 @@ source ~/.bashrc
 mamba activate  dl_torch
 cd /home/ofourkioti/Projects/tmi2022/
 
-export CUDA_VISIBLE_DEVICES=0
 
-python main.py --n_class 2 --data_path "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/" \
---train_set "cam_16_splits/train_0.txt" --val_set "cam_16_splits/val_0.txt" --model_path "graph_transformer/saved_models/" \
+for i in {0..3};
+do CUDA_VISIBLE_DEVICES=0 python main.py --n_class 2 --data_path "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/" \
+--train_set "cam_16_splits/train_${i}.txt" --val_set "cam_16_splits/val_${i}.txt" --model_path "graph_transformer/saved_models/" \
 --log_path "graph_transformer/runs/" \
---task_name "train_cam16_tile_features_0" \
+--task_name "train_cam16_tile_features_${i}" \
 --batch_size 2 \
 --train \
 --log_interval_local 5
+done
