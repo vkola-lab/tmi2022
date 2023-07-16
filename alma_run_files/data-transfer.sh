5d97556278902b9a5fdf76381404df0b0d486ce8
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-07-16 15:44:24 +0100

    transfer

--- alma_run_files/data-transfer.sh
+++ alma_run_files/data-transfer.sh
@@ -4,9 +4,9 @@
 #SBATCH --error=/home/ofourkioti/Projects/tmi2022/results/datatransfer_test.err
 #SBATCH --partition=data-transfer
 #SBATCH --ntasks=1
-#SBATCH --time=48:00:00
+#SBATCH --time=100:00:00
 
 #srun rsync -avP   /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/ovarian_cancer/patches/* /data/rds/DBI/DUDBI/DYNCESYS/OlgaF/ovarian_cancer/patches/
 
-srun rsync -avP /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/tiles/* /data/rds/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/tiles/
-#srun rsync -avP /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/*  /data/rds/DBI/DUDBI/DYNCESYS/OlgaF/slides/
+#srun rsync -avP /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/tiles/* /data/rds/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/tiles/
+srun rsync -avP /data/rds/DBI/DUDBI/DYNCESYS/OlgaF/slides/camelyon_slides/*  /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/
