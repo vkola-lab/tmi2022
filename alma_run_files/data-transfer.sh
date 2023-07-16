75df7f57ea6ff13bac07d2837177c682c5cbdc66
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-07-16 18:51:04 +0100

    transfer

--- alma_run_files/data-transfer.sh
+++ alma_run_files/data-transfer.sh
@@ -9,4 +9,4 @@
 #srun rsync -avP   /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/ovarian_cancer/patches/* /data/rds/DBI/DUDBI/DYNCESYS/OlgaF/ovarian_cancer/patches/
 
 #srun rsync -avP /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/tiles/* /data/rds/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/tiles/
-srun rsync -avP /data/rds/DBI/DUDBI/DYNCESYS/OlgaF/slides/camelyon_slides/*  /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/
+srun rsync -avP /data/rds/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/gtp-feats/*  /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/
