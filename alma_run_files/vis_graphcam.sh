54db3d194907f778ce3ffc92c7859160422adf59
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-07-19 14:28:20 +0100

    transfer

--- alma_run_files/vis_graphcam.sh
+++ alma_run_files/vis_graphcam.sh
@@ -15,8 +15,4 @@ source ~/.bashrc
 mamba activate  dl_torch
 cd /home/ofourkioti/Projects/tmi2022/
 
-python src/vis_graphcam.py \
---path_file "cam_16_splits/test_0.txt"  \
---path_patches "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/tiles"\
---path_WSI "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/camelyon16" \
---path_graph "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/graphs/simclr_files/"
+python src/vis_graphcam.py --path_file "cam_16_splits/test_0.txt"  --path_patches "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/tiles" --path_WSI "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/camelyon16" --path_graph "/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16/graphs/simclr_files/"
