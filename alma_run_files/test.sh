5b857c15a37bd9d72ecf8d64ba7419b0daba04d0
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-07-19 12:02:41 +0100

    test

--- alma_run_files/test.sh
+++ alma_run_files/test.sh
@@ -16,18 +16,18 @@ mamba activate  dl_torch
 cd /home/ofourkioti/Projects/tmi2022/
 
 
-for i in {0..3};
+for i in {0..4};
 do export CUDA_VISIBLE_DEVICES=0
 python main.py \
 --n_class 2 \
 --data_path '/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16' \
---val_set "cam_16_splits/test_${i}.txt" \
+--val_set "cam_16_splits/val_${i}.txt" \
 --model_path "graph_transformer/saved_models/" \
 --log_path "graph_transformer/runs/" \
 --task_name "train_cam16_tile_features_${i}"  \
 --batch_size 1 \
 --test \
 --log_interval_local 5 \
---resume "graph_transformer/saved_models/train_cam16_tile_features_${i}..pth"
+--resume "graph_transformer/saved_models/train_cam16_tile_features_${i}.pth"
 
 done
