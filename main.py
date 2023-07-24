6dc3500c951068140f47245cd7b8cce9e64ea7f5
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-07-24 18:50:46 +0100

    test

--- main.py
+++ main.py
@@ -133,8 +133,8 @@ for epoch in range(num_epochs):
 
                 preds, labels, _ ,out= evaluator.eval_test(sample_batched, model, graphcam)
                 slide_labels.append(labels.cpu().numpy())
-                slide_preds.append(preds.cpu().numpy()[0].tolist())
-                slide_probs.append(out.cpu().numpy().tolist())
+                slide_preds.append(preds.cpu().numpy().tolist())
+                slide_probs.append(out.cpu().numpy()[0].tolist())
                 
                 total += len(labels)
 
