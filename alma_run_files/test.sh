7fa84af6e3f52272bb9eac14bc4865714aa684cc
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-07-24 13:31:25 +0100

    alma

--- alma_run_files/test.sh
+++ alma_run_files/test.sh
@@ -4,8 +4,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --time=100:00:00
-#SBATCH --output=/home/ofourkioti/Projects/tmi2022/results/test_cam16.out
-#SBATCH --error=/home/ofourkioti/Projects/tmi2022/results/test_cam16.err
+#SBATCH --output=/home/ofourkioti/Projects/tmi2022/results/test_colon.out
+#SBATCH --error=/home/ofourkioti/Projects/tmi2022/results/test_colon.err
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpuhm
 
@@ -19,14 +19,14 @@ for i in {0..4};
 do export CUDA_VISIBLE_DEVICES=0
 python main.py \
 --n_class 2 \
---data_path '/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/cam-16' \
---val_set "cam_16_splits/test_${i}.txt" \
+--data_path '/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/Colonoscopy/' \
+--val_set "colon_splits/val_${i}.txt" \
 --model_path "graph_transformer/saved_models/" \
 --log_path "graph_transformer/runs/" \
---task_name "train_cam16_tile_features_${i}"  \
+--task_name "colon_${i}$"  \
 --batch_size 1 \
 --test \
 --log_interval_local 5 \
---resume "graph_transformer/saved_models/train_cam16_tile_features_${i}..pth"
+--resume "colon_${i}.pth"
 
 done
