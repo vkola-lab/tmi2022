1f658079b3b502e6445b649a6447f58081a032f2
Carlin Liao <carlin.liao@berkeley.edu>
2024-01-17 12:37:27 -0500

    clean up and generalize GraphDataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,109 +1,121 @@
+"""Dataset class for the graph classification task."""
+
 import os
+from typing import Any
+
 import torch
 import torch.utils.data as data
-import numpy as np
-from PIL import Image, ImageFile
-import random
-from torchvision.transforms import ToTensor
-from torchvision import transforms
-import cv2
-
-import torch.nn.functional as F
+# import numpy as np
+from PIL import ImageFile
+# from torchvision import transforms
+# import torch.nn.functional as F
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-def collate_features(batch):
-    img = torch.cat([item[0] for item in batch], dim = 0)
-    coords = np.vstack([item[1] for item in batch])
-    return [img, coords]
-    
-def eval_transforms(pretrained=False):
-    if pretrained:
-        mean = (0.485, 0.456, 0.406)
-        std = (0.229, 0.224, 0.225)
 
-    else:
-        mean = (0.5,0.5,0.5)
-        std = (0.5,0.5,0.5)
+# def collate_features(batch):
+#     img = torch.cat([item[0] for item in batch], dim=0)
+#     coords = np.vstack([item[1] for item in batch])
+#     return [img, coords]
 
-    trnsfrms_val = transforms.Compose(
-                    [
-                    transforms.Resize(224),
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean = mean, std = std)
-                    ]
-                )
 
-    return trnsfrms_val
+# def eval_transforms(pretrained=False):
+#     if pretrained:
+#         mean = (0.485, 0.456, 0.406)
+#         std = (0.229, 0.224, 0.225)
+
+#     else:
+#         mean = (0.5, 0.5, 0.5)
+#         std = (0.5, 0.5, 0.5)
+
+#     trnsfrms_val = transforms.Compose(
+#         [
+#             transforms.Resize(224),
+#             transforms.ToTensor(),
+#             transforms.Normalize(mean=mean, std=std)
+#         ]
+#     )
+
+#     return trnsfrms_val
+
 
 class GraphDataset(data.Dataset):
     """input and label image dataset"""
 
-    def __init__(self, root, ids, target_patch_size=-1):
-        super(GraphDataset, self).__init__()
-        """
-        Args:
+    def __init__(self,
+                 root: str,
+                 ids: list[str],
+                 classdict: dict[str, int] = {'normal': 0, 'luad': 1, 'lscc': 2},
+                 target_patch_size: int | None = None,
+                 ) -> None:
+        """Create a GraphDataset.
 
-        fileDir(string):  directory with all the input images.
-        transform(callable, optional): Optional transform to be applied on a sample
+        Args:
+            root (str): Path to the dataset's root directory.
+            ids (list[str]): List of ids of the images to load.
+                Each id should be a string in the format "site/graph_name\tlabel".
+            classdict (dict[str, int]): Dictionary mapping the class names to the class indices.
+            target_patch_size (int | None): Size of the patches to extract from the images.
+
+        The dataset directory structure should be as follows:
+        root/
+        └── {site}_features/
+            └── simclr_files/
+                ├── graph1/
+                │   ├── features.pt
+                │   └── adj_s.pt
+                ├── graph2/
+                │   ├── features.pt
+                │   └── adj_s.pt
+                └── ...
         """
+        super(GraphDataset, self).__init__()
         self.root = root
         self.ids = ids
-        #self.target_patch_size = target_patch_size
-        self.classdict = {'normal': 0, 'luad': 1, 'lscc': 2}        #
-        #self.classdict = {'normal': 0, 'tumor': 1}        #
-        #self.classdict = {'Normal': 0, 'TCGA-LUAD': 1, 'TCGA-LUSC': 2}
-        self._up_kwargs = {'mode': 'bilinear'}
+        self.classdict = classdict
 
-    def __getitem__(self, index):
-        sample = {}
+    def __getitem__(self, index: int) -> dict[str, Any]:
         info = self.ids[index].replace('\n', '')
-        file_name, label = info.split('\t')[0].rsplit('.', 1)[0], info.split('\t')[1]
-        site, file_name = file_name.split('/')
+        try:
+            graph_name, label = info.split('\t')[0].rsplit('.', 1)[0], info.split('\t')[1]
+            site, graph_name = graph_name.split('/')
+        except ValueError as exc:
+            raise ValueError(
+                f"Invalid id format: {info}. Expected format is 'site/filename\tlabel'") from exc
 
-        # if site =='CCRCC':
-        #     file_path = self.root + 'CPTAC_CCRCC_features/simclr_files'
-        if site =='LUAD' or site =='LSCC':
+        if site in {'LUAD', 'LSCC'}:
             site = 'LUNG'
-        file_path = self.root + 'CPTAC_{}_features/simclr_files'.format(site)       #_pre# with # rushin
+            graph_path = os.path.join(self.root, 'CPTAC_{}_features'.format(site))
+        elif site == 'NLST':
+            graph_path = os.path.join(self.root, 'NLST_Lung_features')
+        elif site == 'TCGA':
+            graph_name = info.split('\t')[0]
+            _, graph_name = graph_name.split('/')
+            graph_path = os.path.join(self.root, 'TCGA_LUNG_features')
+        else:
+            graph_path = os.path.join(self.root, f'{site}_features')
+        graph_path = os.path.join(graph_path, 'simclr_files')
 
-        # For NLST only
-        if site =='NLST':
-            file_path = self.root + 'NLST_Lung_features/simclr_files'
-
-        # For TCGA only
-        if site =='TCGA':
-            file_name = info.split('\t')[0]
-            _, file_name = file_name.split('/')
-            file_path = self.root + 'TCGA_LUNG_features/simclr_files'       #_resnet_with
-
+        sample: dict[str, Any] = {}
         sample['label'] = self.classdict[label]
-        sample['id'] = file_name
+        sample['id'] = graph_name
 
-        #feature_path = os.path.join(self.root, file_name, 'features.pt')
-        feature_path = os.path.join(file_path, file_name, 'features.pt')
-
+        feature_path = os.path.join(graph_path, graph_name, 'features.pt')
         if os.path.exists(feature_path):
             features = torch.load(feature_path, map_location=lambda storage, loc: storage)
         else:
-            print(feature_path + ' not exists')
-            features = torch.zeros(1, 512)
+            raise FileNotFoundError(f'features.pt for {graph_name} doesn\'t exist')
 
-        #adj_s_path = os.path.join(self.root, file_name, 'adj_s.pt')
-        adj_s_path = os.path.join(file_path, file_name, 'adj_s.pt')
+        adj_s_path = os.path.join(graph_path, graph_name, 'adj_s.pt')
         if os.path.exists(adj_s_path):
             adj_s = torch.load(adj_s_path, map_location=lambda storage, loc: storage)
         else:
-            print(adj_s_path + ' not exists')
-            adj_s = torch.ones(features.shape[0], features.shape[0])
+            raise FileNotFoundError(f'adj_s.pt for {graph_name} doesn\'t exist')
 
-        #features = features.unsqueeze(0)
         sample['image'] = features
-        sample['adj_s'] = adj_s     #adj_s.to(torch.double)
-        # return {'image': image.astype(np.float32), 'label': label.astype(np.int64)}
+        sample['adj_s'] = adj_s
 
         return sample
-
 
     def __len__(self):
         return len(self.ids)
